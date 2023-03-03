103f5c2f9d2002f40585b7e97a25ce83aca71204
EdisonAlzate <edisonalzate11@gmail.com>
2023-03-03 15:38:18 -0500

    create react app init

---
+++ chat/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container mt-3'>
+        <div className='card'>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;

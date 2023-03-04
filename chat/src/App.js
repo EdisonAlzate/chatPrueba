c9e9c99ff6b20928861786f4c7ce6c7244a4894f
EdisonAlzate <edisonalzate11@gmail.com>
2023-03-03 19:42:42 -0500

    Frontend finish

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,48 +1,125 @@
 import './App.css';
 import io from 'socket.io-client';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 //listen  events 
 const socket = io('http://localhost:4000')
 
 function App() {
+  const [nickname, setNickname] = useState('')
+  const [message, setMessage] = useState('')
+  const [disabled, setDisabled] = useState('false')
+  const [messages, setMessages] = useState([])
+
+  const [storedMessages, setStoredMessages] = useState([])
+  const [firtTime, setFirtTime] = useState(false)
+
+  const url = 'http://localhost:4000/api/'
+  useEffect(() => {
+    const messageReceved = (message) => {
+      setMessages([message, ...messages])
+    }
+
+    socket.on('message', messageReceved)
+    return () => {
+      socket.off('message', messageReceved)
+    }
+
+  }, [messages])
+
+  if (!firtTime) {
+    axios.get(url + "messages").then(res => {
+      setStoredMessages(res.data.messages)
+    })
+    setFirtTime(true)
+  }
+
+
+  const nicknameSubmit = (e) => {
+    e.preventDefault()
+    setNickname(nickname)
+    setDisabled(true)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (nickname !== '') {
+      socket.emit('message', message, nickname)
+
+      const newMessage = {
+        body: message,
+        from: 'I Am'
+      }
+      setMessages([newMessage, ...messages])
+      setMessage('')
+
+      //axios http POST to save message
+      axios.post(url + 'save', {
+        message: message,
+        from: nickname
+      })
+    } else {
+      alert('To send a message should add a nickname')
+    }
+  }
+
   return (
     <div className="App">
       <div className='container mt-3'>
-        <div className='card'>
+
+        <div className="card shadow border-0">
           <div className='card-body'>
             <h5 className="text-center">CHAT KUEPA</h5>
 
             {/*use*/}
-            <form>
+            <form onSubmit={nicknameSubmit}>
               <div className="d-flex mb-3">
-                <input type="text" className='form-control' placeholder='Usuario...' id="usuario" />
-                <button className="btn btn-success mx-3" type="submit" id="btn-usuario">Add</button>
+                <input type="text" className='form-control' id="nickname" placeholder="Nickname..." onChange={e => setNickname(e.target.value)} value={nickname} required />
+                <button className="btn btn-success mx-3" type="submit" id="btn-nickname" >Add</button>
               </div>
             </form>
 
             {/*chat User*/}
-            <form>
+            <form onSubmit={submitHandler}>
               <div className="d-flex">
-                <input type="text" className='form-control' placeholder='Message...' id="message" />
+                <input type="text" className='form-control' placeholder='Message...' id="message" onChange={e => setMessage(e.target.value)} value={message} />
                 <button className="btn btn-success mx-3" type="submit" id="btn-message">Send</button>
               </div>
             </form>
-
-            {/*chat Messages*/}
-
           </div>
         </div>
-        <div className='card mt-3 mb-3' id="content-chats">
+
+        {/*chat Messages*/}
+        <div className='card mt-3 mb-3 shadow border-0' id="content-chat">
           <div className='card-body'>
 
+            {messages && messages.map((message, index) => (
+              <div key={index} className={`d-flex p-3 ${message.from === "I Am" ? "justify-content-end" : "justify-content-start"}`}>
+                <div className={`card mb-3 shadow border-1${message.from === "I Am" ? "bg-success bg-opacity-25" : "bg-ligth"}`}>
+                  <div className='card-body'>
+                    <small className=''>{message.from}:{message.body}</small>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {/*chat stored Messages*/}
+            <small small className='text-center text-muted' >...Messagges saved...</small>
+            {storedMessages && storedMessages.map((message, index) => (
+              <div key={index} className={`d-flex p-3 ${message.from === nickname ? "justify-content-end" : "justify-content-start"}`}>
+                <div className={`card mb-3 shadow border-1 ${message.from === nickname ? "bg-success bg-opacity-25" : "bg-ligth"}`}>
+                  <div className='card-body'>
+                    <small className='text-muted'>{message.from}:{message.message}</small>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-
       </div>
-
     </div>
-  );
+  )
+
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import morgan from "morgan"
-import {Server as Socketserver} from 'socket.io'
+import { Server as Socketserver } from 'socket.io'
 import http from 'http'
 import cors from 'cors'
 import mongoose from "mongoose"
@@ -10,39 +10,47 @@ import { Socket } from "socket.io-client"
 
 //Config Mongoose
 
-let email='edisonalzate11@gmail.com'
-var urlToConnectionMongo='mongodb+srv://edisonalzate11:FSAem7DyXBauLWNR@cluster0.vxwviub.mongodb.net/?retryWrites=true&w=majority'
-mongoose.Promise=global.Promise
+let email = 'edisonalzate11@gmail.com'
+var urlToConnectionMongo = 'mongodb+srv://edisonalzate11:FSAem7DyXBauLWNR@cluster0.vxwviub.mongodb.net/?retryWrites=true&w=majority'
+mongoose.Promise = global.Promise
 
-const app=express()
-const PORT=4000
+const app = express()
+const PORT = 4000
 
 // WE WILL CREATE A SERVER WITH  HTTP
-const server =http.createServer(app)
-const io =new Socketserver(server,{
-    cors:{
-        origin:'*'
+const server = http.createServer(app)
+const io = new Socketserver(server, {
+    cors: {
+        origin: '*'
     }
 })
 
 //We Will create Middlewares
 app.use(cors())
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/api',router)
+app.use('/api', router)
 
-io.on('connection',(socket)=>{
-    console.log("socket.id",socket.id)
+io.on('connection', (socket) => {
+    console.log("socket.id", socket.id)
     console.log("client connected")
+    socket.on('message', (message, nickname) => {
+        //send other clients online
+        socket.broadcast.emit('message', {
+            body: message,
+            from: nickname
+        })
+
+    })
 })
 
 
 // Connect to DataBase
-mongoose.connect(urlToConnectionMongo,{useNewUrlParser:true}).then(()=>{
+mongoose.connect(urlToConnectionMongo, { useNewUrlParser: true }).then(() => {
     console.log('Data Base conneted succesfully')
-    server.listen(PORT,()=>{
-        console.log('Server runing in http://localhost:',PORT)
+    server.listen(PORT, () => {
+        console.log('Server runing in http://localhost:', PORT)
     })
 })
 
